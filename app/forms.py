202a649e240ea1baebb4b22af8c0a1f53603efe6
Andre Lashley <andre.lashley@gmail.com>
2022-07-09 16:38:02 -0700

    Implement follow and unfollow

--- app/forms.py
+++ app/forms.py
@@ -42,5 +42,8 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError('Please use a different username.')
 
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
 
 
